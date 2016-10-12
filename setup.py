470503a689f9b0f7a737d24762a97e7e90eb3c83
hinest <treverhines@gmail.com>
2016-10-11 21:17:11 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+if __name__ == '__main__':
+  from numpy.distutils.core import setup
+  setup(name='PyTDE',
+        version='1.0',
+        author='Trever Hines',
+        author_email='treverhines@gmail.com',
+        packages=['pytde'],
+        license='MIT')
+
